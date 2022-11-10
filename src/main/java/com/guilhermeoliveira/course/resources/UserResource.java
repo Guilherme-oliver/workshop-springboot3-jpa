4154b1b29ca77bbdb777ece8383b5fd5dd4a300b
Guilherme <guilherme.rodriguess.oliveira@gmail.com>
2022-11-10 14:06:49 -0300

    H2 database, test profile, JPA

--- src/main/java/com/guilhermeoliveira/course/resources/UserResource.java
+++ src/main/java/com/guilhermeoliveira/course/resources/UserResource.java
@@ -1,5 +1,22 @@
 package com.guilhermeoliveira.course.resources;
 
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+import com.guilhermeoliveira.course.entities.User;
+
+@RestController
+@RequestMapping(value = "/users")
 public class UserResource {
+	
+	@GetMapping
+	public ResponseEntity<User> findAll(){
+		
+		User user = new User(1L, "Maria", "maria@gmail.com", "808080", "123456");
+		return ResponseEntity.ok().body(user);
+		
+	}
 
 }
