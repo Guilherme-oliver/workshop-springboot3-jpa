84738a6420f1ed34dfda982c69971cf470fe98eb
Guilherme <guilherme.rodriguess.oliveira@gmail.com>
2022-11-18 14:57:54 -0300

    Payment, one-to-one association

--- src/main/java/com/guilhermeoliveira/course/config/TestConfig.java
+++ src/main/java/com/guilhermeoliveira/course/config/TestConfig.java
@@ -11,6 +11,7 @@ import org.springframework.context.annotation.Profile;
 import com.guilhermeoliveira.course.entities.Category;
 import com.guilhermeoliveira.course.entities.Order;
 import com.guilhermeoliveira.course.entities.OrderItem;
+import com.guilhermeoliveira.course.entities.Payment;
 import com.guilhermeoliveira.course.entities.Product;
 import com.guilhermeoliveira.course.entities.User;
 import com.guilhermeoliveira.course.entities.enums.OrderStatus;
@@ -81,6 +82,10 @@ public class TestConfig implements CommandLineRunner{
 		
 		orderItemRepository.saveAll(Arrays.asList(oi1, oi2, oi3, oi4));
 		
+		Payment pay1 = new Payment(null, Instant.parse("2019-06-20T21:53:07Z"), o1);
+		o1.setPayment(pay1);
+		
+		orderRepository.save(o1);
 	}
 
 }
